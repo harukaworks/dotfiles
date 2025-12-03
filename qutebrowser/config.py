5d16a4847fa1956c520b86fb19995c33785fe0fe
singal <singalmeng@163.com>
2025-12-03 14:02:25 +0800

    first commit

---
+++ qutebrowser/config.py
@@ -0,0 +1,48 @@
+config.load_autoconfig(False)
+c.fonts.default_size = "12pt"
+c.content.headers.accept_language = "zh-CN;q=0.9,en-US,en;q=0.8"
+# c.content.headers.user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, Like Gecko) Chrome/120.0.0.0 Safari/537.36"
+
+c.hints.padding = {"bottom":1, "left":5, "right":3, "top":1}
+c.hints.border = "none" 
+c.colors.hints.bg = "#2c2c2c00"
+c.colors.hints.fg = "#f0c674"
+
+c.scrolling.smooth = True
+c.statusbar.widgets = ["search_match", "text:|", "url", "text:|", "scroll"]
+c.statusbar.padding = {"bottom":2, "left":5, "right":5, "top":2}
+c.tabs.padding = {"bottom":2, "left":5, "right":5, "top":2}
+c.colors.tabs.even.bg = "#282a2e"
+c.colors.tabs.even.fg = "#d2d4d4"
+
+c.colors.tabs.odd.bg = "#282a2e"
+c.colors.tabs.odd.fg = "#d2d4d4"
+
+c.colors.tabs.selected.even.bg = "#f0c674"
+c.colors.tabs.selected.even.fg = "#282a2e"
+
+c.colors.tabs.selected.odd.bg = "#f0c674"
+c.colors.tabs.selected.odd.fg = "#282a2e"
+
+
+c.url.default_page = "https://bstf.tj.cn/home.html"
+c.url.start_pages = "https://bstf.tj.cn/home.html"
+c.url.searchengines = {"DEFAULT": "https://www.baidu.com/s?ie=utf-8&rsv+bp=1&rsvidx=1&tn=baidu&wd={}"}
+
+c.fonts.web.family.standard = "Noto Sans CJK SC"
+c.fonts.web.family.sans_serif = "Noto Sans CJK SC"
+c.fonts.web.family.serif = "Noto Serif CJK SC"
+c.fonts.web.family.fixed = "Noto Sans Mono CJK SC"
+
+
+c.colors.webpage.darkmode.enabled = True
+c.colors.webpage.bg = "#282a2e"
+c.colors.statusbar.progress.bg = "#373b41"
+c.colors.statusbar.normal.bg = "#282a2e"
+c.colors.statusbar.url.success.https.fg = "#d2d4d4"
+c.colors.statusbar.url.success.http.fg = "#d2d4d4"
+c.input.insert_mode.auto_load = True
+c.colors.statusbar.insert.bg = "#373b41"
+c.colors.statusbar.insert.fg = "#d2d4d4"
+
+
